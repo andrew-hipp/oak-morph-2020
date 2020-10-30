d8722ad46001bcf0188a6d26dab14b5bf242a594
Andrew Hipp <AHipp@MORTON.LOCAL>
2020-10-30 10:26:55 -0500

    Updating map figure Cleaned up for R1

--- scripts/01.worldclim-and-maps.R
+++ scripts/01.worldclim-and-maps.R
@@ -7,23 +7,21 @@ doState = FALSE
 if(!exists('worldclim')) worldclim <- getData("worldclim",var="bio",res=10)
 oak.coord <- SpatialPoints(oak.dat[ , c('long', 'lat')])
 
-## whole sampling range.... you need to adjust xlim and ylim to limit yourself to the range you want to plot
-pdf('Fig 1. Map of Sampling Sites.pdf', 5, 6)
-map('world', c('usa', 'canada'), xlim = c(-141, -50), ylim = c(20, 55))
-map('county', add = TRUE, col = 'gray', lwd = 0.25)
-map('state', add = TRUE, col = 'black', lwd = 1)
-map.axes(cex.axis=0.8)
-points(oak.coord, pch = 21, col = 'black', bg = 'red', cex = 1)
-
-dev.off()
-
 ##sampling range with bio1 (mean annual temperature)
-pdf('Fig 1. Map of Sampling Sites.bio1.pdf', 6,5)
-plot(worldclim, y = 'bio1', xlim = c(-150, -50), ylim = c(10, 55), main = "")
-map('county', add = TRUE, col = "white", lwd = 0.5)
-map('state', add = TRUE, col = 'black', lwd = 1)
+pdf('../out/Fig-1.MAP.bio1.pdf', 7.25,6)
+plot(worldclim, y = 'bio1',
+    xlim = c(-110, -60), ylim = c(25, 55),
+    axes = T, box = T, legend = T,
+    main = "",
+    xlab = "Longitude (degrees)",
+    ylab = "Latitude (degrees)",
+    cex.axis = 0.7)
+#map('county', add = TRUE, col = "white", lwd = 0.5)
+map('state', add = TRUE, col = 'black', lwd = 1, bty = 'n',
+xlim = c(-110, -60), ylim = c(25, 55),)
 points(oak.coord, pch = 21, col = 'black', bg = 'red', cex = 1)
-
+# axis(1, cex = 0.5)
+# axis(2, cex = 0.5)
 dev.off()
 
 ## a single state example
