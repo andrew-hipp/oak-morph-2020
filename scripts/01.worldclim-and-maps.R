f056bbfa01d32bc7e749c8d05f53470427725bf2
Andrew Hipp <AHipp@MORTON.LOCAL>
2020-10-30 10:28:06 -0500

    Cleaning up map code getting rid of a few lines that were just for development

--- scripts/01.worldclim-and-maps.R
+++ scripts/01.worldclim-and-maps.R
@@ -2,8 +2,6 @@ library(raster)
 library(sp)
 library(maps)
 
-doState = FALSE
-
 if(!exists('worldclim')) worldclim <- getData("worldclim",var="bio",res=10)
 oak.coord <- SpatialPoints(oak.dat[ , c('long', 'lat')])
 
@@ -16,21 +14,10 @@ plot(worldclim, y = 'bio1',
     xlab = "Longitude (degrees)",
     ylab = "Latitude (degrees)",
     cex.axis = 0.7)
-#map('county', add = TRUE, col = "white", lwd = 0.5)
 map('state', add = TRUE, col = 'black', lwd = 1, bty = 'n',
 xlim = c(-110, -60), ylim = c(25, 55),)
 points(oak.coord, pch = 21, col = 'black', bg = 'red', cex = 1)
-# axis(1, cex = 0.5)
-# axis(2, cex = 0.5)
 dev.off()
-
-## a single state example
-if(doState) {
-  map('county', 'oklahoma', lwd = 0.25, col = 'gray')
-  map('state', 'oklahoma', add = T)
-  points(oak.coord, pch = 21, col = 'black', bg = 'red', cex = 1)
-}
-## and you might want to do a single site
 
 ## now let's get climatic data:
 oak.clim <- extract(worldclim, oak.coord)
