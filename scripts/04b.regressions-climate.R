c56a5d23248d860e70a17c3f3e2091eb61fcf297
Andrew Hipp <AHipp@MORTON.LOCAL>
2020-11-01 20:23:17 -0600

    Climate regressions (04b) pretty well cleaned up

--- scripts/04b.regressions-climate.R
+++ scripts/04b.regressions-climate.R
@@ -1,6 +1,9 @@
 #CLIMATE REGRESSIONS -- fig 5
 # summary(lm(bio1 ~ lat , data = oak.means))
 
+library(ggplot2)
+library(ggrepel)
+library(gridExtra)
 
 p10 <- ggplot(oak.means.se, aes(x=lat, y=bio1))
 p10 <- p10 + geom_point()
@@ -9,9 +12,11 @@ p10 <- p10 + geom_errorbar(aes(ymin = bio1-bio1.se, ymax = bio1+bio1.se),
                        width = 0.2)
 p10 <- p10 + labs(x = '',
               y = 'bio1: mean annual temperature')
-p10 <- p10 + annotate("text", x = 42, y = 150,
-                  label = "p < 0.001, r2 = 0.9926",
-                  hjust = 0)
+p10 <- p10 + annotate("text",
+                  max(oak.means.se$lat),
+                  max(oak.means.se$bio1 + oak.means.se$bio1.se),
+                  label = "p < 0.001, r2 = 0.99",
+                  hjust = 'right', size = 2)
 p10 <- p10 + geom_label_repel(size = 1, aes(label = site))
 
 p11 <- ggplot(oak.means.se, aes(x=lat, y=bio12))
@@ -21,9 +26,11 @@ p11 <- p11 + geom_errorbar(aes(ymin = bio12-bio12.se, ymax = bio12+bio12.se),
                        width = 0.2)
 p11 <- p11 + labs(x = '',
               y = 'bio12: mean annual precipitation')
-p11 <- p11 + annotate("text", x = 43, y = 1050,
+p11 <- p11 + annotate("text",
+                  max(oak.means.se$lat),
+                  max(oak.means.se$bio12 + oak.means.se$bio12.se),
                   label = "P < 0.001, r2 = 0.68",
-                  hjust = 0)
+                  hjust = 'right', size = 2)
 
 p12 <- ggplot(oak.means.se, aes(x=lat, y=bio4))
 p12 <- p12 + geom_point()
@@ -32,11 +39,13 @@ p12 <- p12 + geom_errorbar(aes(ymin = bio4-bio4.se, ymax = bio4+bio4.se),
                        width = 0.2)
 p12 <- p12 + labs(x = 'Latitude (degrees)',
               y = 'bio4: temperature seasonality')
-p12 <- p12 + annotate("text", x = 43, y = 9500,
-                  label = "P < 0.001, r2 = 0.98",
-                  hjust = 0)
+p12 <- p12 + annotate("text",
+                      min(oak.means.se$lat),
+                      max(oak.means.se$bio4 + oak.means.se$bio4.se),
+                      label = "P < 0.001, r2 = 0.98",
+                      hjust = 'left', size = 2)
 
-pdf("../out/Fig 5. Climate.Data.Panel.pdf", 3.5, 9)
+pdf("../out/Fig5.climateRegressions.pdf", 3.5, 9)
 
 grid.arrange(p10, p11, p12)
 
